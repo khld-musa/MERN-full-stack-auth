2dfb8203525610865fe372f7d4590fae84c03309
khld-musa <elex22.sust@gmail.com>
2021-09-22 10:07:31 -0700

    front-end-update

---
+++ client/src/components/mainpages/utils/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            hello khld
+        </div>
+    )
+}
+
+export default NotFound
---
+++ client/src/components/mainpages/utils/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function profile() {
+    return (
+        <div>
+            404 | Not Found
+        </div>
+    )
+}
+
+export default profile
